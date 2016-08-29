e6103487facda79791783704c7c43d2e3fae5ac7
Brett Macy <bam03584@aol.com>
2016-08-29 14:45:33 -0400

    should have Rebass dropdown and navigation to game

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Select } from 'rebass'
 
 class Home extends Component {
   _handleClick = () => {
@@ -6,8 +7,21 @@ class Home extends Component {
   }
   render () {
     return <div className="Home">
-      <button onClick={this._handleClick}>StartSweeping</button>
+    <h1>Welcome to BombSniffer&trade;</h1>
+      <Select
+        label="Select Your Difficulty"
+        name="Difficulty Level"
+        options={[{children: 'Hard', value: 2}, {children: 'Medium', value: 1}, {children: 'Easy', value: 0}]}
+        rounded
+      />
+      <br />
+      <button onClick={this._handleClick}>Happy Sweeping!</button>
     </div>
   }
 }
+
+Home.propTypes = {
+  navigate: React.PropTypes.func.isRequired
+}
+
 export default Home
